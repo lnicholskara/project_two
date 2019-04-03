bf316ef15c43a45899c99a7b19257d84543092e9
Kara Nichols <lnicholskara@gmail.com>
2019-04-02 17:42:29 -0700

    fix index.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,14 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
+var config = require(__dirname + "/../config/config.js")[env];
 var env = process.env.NODE_ENV || "development";
-var sequelize = new Sequelize(process.env[config.use_env_variable],config);
+var sequelize = new Sequelize(process.env[config.use_env_variable]);
 // var sequelize = new Sequelize(process.env[config.use_env_variable]);
 var db = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable],config);
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(
     config.database,
